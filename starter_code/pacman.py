cfc067bd10f17aab32f68e4499cf64a265661ed6
Dan McAteer <daniel_m@optimove.com>
2019-09-19 22:35:06 -0400

    Create helper funcs
    Create helper funcs to get various board elements

--- starter_code/pacman.py
+++ starter_code/pacman.py
@@ -1,8 +1,10 @@
 """
-Write a module docstring here
+Returns the final_pos_x, final_pos_y, and coins_colected of a pacman
 """
+import os
 
-__author__ = "Your Name"
+__author__ = "Dan McAteer"
+file_path = "test_files\py_test\generic.txt"
 
 
 def pacman(input_file):
@@ -17,5 +19,56 @@ def pacman(input_file):
         2. final_pos_y (int) = final y location of Pacman
         3. coins_collected (int) = the number of coins that have been collected by Pacman across all movements
     """
+    input_arr = read_input(file_path)
+    grid_size = get_grid_size(input_arr)
+    start_pos = get_start_pos(input_arr)
+    walls = get_walls()
+    coins_collected = 0
+    final_pos_x = start_pos[0]
+    final_pos_y = start_pos[1]
 
-    # return final_pos_x, final_pos_y, coins_collected 
+    return final_pos_x, final_pos_y, coins_collected
+
+def read_input(text_file):
+
+    input_file = open(text_file, 'r')
+    lines = input_file.readlines()
+
+    return lines
+
+def get_grid_size(input_arr):
+
+    line_1 = input_arr[0]
+    x_coord, y_coord = map(int, line_1.split(" "))
+
+    return x_coord, y_coord
+
+def get_start_pos(input_arr):
+
+    line_2 = input_arr[1]
+    x_coord, y_coord = map(int, line_2.split(" "))
+
+    return x_coord, y_coord
+
+def get_walls(input_arr):
+
+    walls = []
+    
+    for index in range(3, len(input_arr)):
+        
+        line = input_arr[index]
+        x_coord, y_coord = map(int, line.split(" "))
+        coords = x_coord, y_coord
+        walls.append(coords)
+    
+    return walls
+
+def get_move_value(direction):
+
+    direction_dict = {"N": 1, "E": 1, "S": -1, "W": -1}
+
+    return direction_dict[direction]
+
+
+
+
